6f3657b3a3b1091d7d8e5e6623b6bf1819ff517c
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-10 16:27:53 -0300

    se instaló bootstrap y se eliminaron estilos innecesarios. Se crearon los types y se comenzó a pasar todo el codigo a typescript

--- src/components/Guitar.tsx
+++ src/components/Guitar.tsx
@@ -1,6 +1,12 @@
-const Guitar = ({ guitar, addToCart }) => {
-  const { id, name, description, image, price } = guitar;
+import type { Guitar } from "../types";
 
+type GuitarProps = {
+  guitar: Guitar;
+  addToCart: (item: Guitar) => void;
+};
+
+const Guitar = ({ guitar, addToCart }: GuitarProps) => {
+  const { name, description, image, price } = guitar;
 
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -14,10 +20,10 @@ const Guitar = ({ guitar, addToCart }) => {
       <div className="col-8">
         <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
         <p>{description}</p>
-        <p className="fw-black text-primary fs-3">${price}</p>
+        <p className="fw-bolder text-primary fs-3">${price}</p>
         <button
           type="button"
-          className="btn btn-dark w-100"
+          className="btn btn-dark text-uppercase fw-bold"
           onClick={() => addToCart(guitar)}
         >
           Agregar al Carrito
