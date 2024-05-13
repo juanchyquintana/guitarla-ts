f5bb6eac717b61da3c9b2de7a8769988a7274b11
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-13 14:39:10 -0300

    se agregó useReducer en el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useCart } from "./hooks/useCart.js";
+import { useEffect, useReducer } from "react";
 import Footer from "./components/Footer.js";
 import Guitar from "./components/Guitar.js";
 import Header from "./components/Header.js";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { cartReducer, initialState } from "./reducers/cart-reducer.js";
 
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
 
-  const { cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, data, addToCart, isEmpty, cartTotal } = useCart()
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <>
-      <Header
-        cart={cart}
-        removeFromCart={removeFromCart}
-        increaseQuantity={increaseQuantity}
-        decreaseQuantity={decreaseQuantity}
-        clearCart={clearCart}
-        isEmpty={isEmpty}
-        cartTotal={cartTotal}
-      />
+      <Header cart={state.cart} dispatch={dispatch} />
 
       <main className="container-xl mt-5">
         <h2 className="text-center fs-2">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          {data.map((guitar) => (
-            <Guitar
-              key={guitar.id}
-              guitar={guitar}
-              addToCart={addToCart}
-            />
+          {state?.data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} dispatch={dispatch} />
           ))}
         </div>
       </main>
